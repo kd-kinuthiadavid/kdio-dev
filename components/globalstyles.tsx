53230a411a7458f13ae2d396fa52e9ecdeb0a6c7
KINUTHIA DAVID <kd.kinuthiadavid@gmail.com>
2022-09-28 17:19:23 +0300

    fix(build): move the importing of google fonts to '_document.tsx'
    Apparently, we can't use @import in 'createGlobalStyles' right now due
    to how nextjs handles the CSSAPI.
    
    Instead, we should create a custom document and use a css <link /> to
    inject the fonts.
    
    @see https://nextjs.org/docs/messages/no-stylesheets-in-head-component
    
    This fixes an issue where fonts and background colors were not being
    loaded in production.

--- components/globalstyles.tsx
+++ components/globalstyles.tsx
@@ -1,9 +1,6 @@
 import { createGlobalStyle } from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
-  * {
-    @import url('https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@100;200;300;400;500;600;700;800&family=Space+Grotesk:wght@300;400;500;600;700&display=swap');
-  }
   html,
   body {
     color: ${({ theme }) => theme.colors.text};
